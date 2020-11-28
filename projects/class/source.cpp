a13dbba5604458dc9f7afe0d4a64a047785d8663
arshak <Arshak-Yesayan>
2020-11-28 12:19:42 +0400

    fix

--- projects/class/source.cpp
+++ projects/class/source.cpp
@@ -3,31 +3,32 @@
 
 using namespace std;
 
-double get_distance(int x_1, int y_1, int x_2, int y_2) {
-    return sqrt( pow(x_1 - x_2, 2) + pow(y_1 - y_2, 2) );
+double get_distance(int pos_1[2], int pos_2[2]) { // pos_1[2] = {x_1, y_1}; pos_2[2] = {x_2, y_2}
+    return sqrt( pow(pos_1[0] - pos_2[0], 2) + pow(pos_1[1] - pos_2[1], 2) );
 }
 
 int main() {
-    int n, r;
-    int arr[200];
-    int max_pos_1, max_pos_2;
-    double max_distance = -1;
+    int n, r, i, j, arr[100][2], near_1 = 0, near_2 = 1;
+    double min_distance, new_distance;
 
     cin >> n >> r;
+    cin >> arr[0][0] >> arr[0][1];
+    cin >> arr[1][0] >> arr[1][1];
+    min_distance = get_distance(arr[0], arr[1]);
 
-    for(int i = 0; i < n; i++) {
-        cin >> arr[2 * i] >> arr[2 * i + 1];
-        for(int j = 0; j < i; j++) {
-            double new_distance = get_distance(arr[2 * i], arr[2 * i + 1], arr[2 * j], arr[2 * j + 1]);
-            if(max_distance == -1 || max_distance > new_distance) {
-                max_pos_1 = j;
-                max_pos_2 = i;
-                max_distance = new_distance;
+    for(i = 2; i < n; i++) {
+        cin >> arr[i][0] >> arr[i][1];
+        for(j = 0; j < i; j++) {
+            new_distance = get_distance(arr[i], arr[j]); // arr[i] = {x_i, y_i}; arr[j] = {x_j, y_j}
+            if(min_distance >= new_distance) {
+                near_1 = j;
+                near_2 = i;
+                min_distance = new_distance;
             }
         }
     }
 
-    cout << max_pos_1 << " " << max_pos_2 << endl;
+    cout << near_1 << " " << near_2 << endl;
 
     return 0;
 }

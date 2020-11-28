f6982eb48807effb358ae4a0d0a29466bd88978f
arshak <Arshak-Yesayan>
2020-11-28 11:49:33 +0400

    fix

--- projects/class/source.cpp
+++ projects/class/source.cpp
@@ -4,23 +4,25 @@
 using namespace std;
 
 double get_distance(int x_1, int y_1, int x_2, int y_2) {
-    return  sqrt( pow(x_1 - x_2, 2) + pow(y_1 - y_2, 2) );
+    return sqrt( pow(x_1 - x_2, 2) + pow(y_1 - y_2, 2) );
 }
 
 int main() {
     int n, r;
     int array[200];
     int max_pos_1, max_pos_2;
-    double max_distance = 0;
+    double max_distance = -1;
 
     cin >> n >> r;
 
     for(int i = 0; i < n; i++) {
         cin >> array[2 * i] >> array[2 * i + 1];
         for(int j = 0; j < i; j++) {
-            if(max_distance <= get_distance(array[2 * i], array[2 * i + 1], array[2 * j], array[2 * j + 1])) {
+            double new_distance = get_distance(array[2 * i], array[2 * i + 1], array[2 * j], array[2 * j + 1]);
+            if(max_distance == -1 || max_distance > new_distance) {
                 max_pos_1 = j;
                 max_pos_2 = i;
+                max_distance = new_distance;
             }
         }
     }
